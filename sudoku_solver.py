283eebf3c33121f6a33158d5344a026eeb02f052
rishivimal <rvimalen@uwaterloo>
2022-01-17 19:28:41 -0500

    reformat

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -1,10 +1,10 @@
 from tkinter import *
-import numpy as np
 from tkinter import ttk
+
+import numpy as np
 
 
 def find_empty(board):
-
     for row2 in range(len(board)):
         for element in range(len(board[1])):
             if board[row2][element] == 0:
@@ -13,7 +13,6 @@ def find_empty(board):
 
 
 def solve(board):
-
     solved_board = board
     possible_answers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
@@ -34,7 +33,6 @@ def solve(board):
             solved_board[empty_x][empty_y] = answer
 
             if solve(solved_board):
-
                 write_answers(solved_board)
                 return True
 
@@ -44,7 +42,6 @@ def solve(board):
 
 
 def check_initial(board):
-
     for row2 in range(0, 9):
         st = set()
         for element in range(0, 9):
@@ -61,13 +58,12 @@ def check_initial(board):
             else:
                 st.add(board[row2][column])
 
-
     return True
 
 
 def is_valid(board, row2, column, check):
-
-    if row_check(board, row2, check) and column_check(board, column, check) and box_check(board, row2 - row2 % 3, column-column % 3, check):
+    if row_check(board, row2, check) and column_check(board, column, check) and box_check(board, row2 - row2 % 3,
+                                                                                          column - column % 3, check):
         return True
 
     else:
@@ -75,7 +71,6 @@ def is_valid(board, row2, column, check):
 
 
 def row_check(board, row2, check):
-
     last_element = 0
     for element in board[row2]:
         if check == element:
@@ -90,7 +85,6 @@ def row_check(board, row2, check):
 
 
 def column_check(board, column, check):
-
     last_element = 0
     for row2 in board:
         if check == row2[column]:
@@ -105,7 +99,6 @@ def column_check(board, column, check):
 
 
 def box_check(board, start_row, start_col, check):
-
     box_length = 3
     last_element = 0
 
@@ -123,12 +116,10 @@ def box_check(board, start_row, start_col, check):
 
 
 root = Tk()
-root.title ("Sudoku solver")
-
+root.title("Sudoku solver")
 
 title = Label(root, text="Enter known values")
-title.grid(row =0, column=4)
-
+title.grid(row=0, column=4)
 
 size = [0, 1, 2, 3, 4, 5, 6, 7, 8]
 
@@ -139,11 +130,10 @@ for row in size:
         ent = Entry(root, width=5)
         ent.insert(END, "0")
         entry_list.append(ent)
-        ent.grid(row=row+1, column=col)
+        ent.grid(row=row + 1, column=col)
 
 
-def create_board ():
-
+def create_board():
     board_list = []
     for entry in entry_list:
         board_list.append(int(entry.get()))
@@ -153,32 +143,32 @@ def create_board ():
         unsolvable()
 
 
-def write_answers (board):
-
+def write_answers(board):
     answer_list = []
     for row in board:
         for col in row:
             answer_list.append(col)
 
-    ctr=0
+    ctr = 0
 
     for entry in entry_list:
         entry.delete(0, END)
         entry.insert(END, answer_list[ctr])
         ctr += 1
 
-def clear_board():
 
+def clear_board():
     for entry in entry_list:
-        entry.delete (0, END)
+        entry.delete(0, END)
         entry.insert(END, "0")
 
-def unsolvable():
 
+def unsolvable():
     label = Label(root, text="This puzzle is unsolvable")
     label.grid(row=11, column=0)
     label.after(5000, lambda: label.destroy())
     clear_board()
+
 
 solve_button = ttk.Button(root, text="Solve", command=create_board)
 solve_button.grid(row=10, column=0)
@@ -187,6 +177,3 @@ clear_button = ttk.Button(root, text="Clear", command=clear_board)
 clear_button.grid(row=10, column=1)
 
 root.mainloop()
-
-
-
